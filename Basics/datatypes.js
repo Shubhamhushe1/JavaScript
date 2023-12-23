5e1173c9f3b9f9ff569f0f62805b17baa65d50cd
Shubham Hushe <101410703+Shubhamhushe1@users.noreply.github.com>
2023-12-23 18:22:00 +0000

    datatypebasics and typeof command use

---
+++ Basics/datatypes.js
@@ -0,0 +1,28 @@
+// datatypes in javascript
+
+// Premitive Datatypes
+
+// 1. String      let name = "Ram" It can store words etc
+// 2. Number      let id = 01      It can store numric values
+// 3. Bigint      let accountNumber = (1234567890) Bigint is use to store large integer or numaric values in variables  
+// 4. Boolean     It is use for True & False or 1 = True and 0 = False  
+// 5. Undefined   let x;   It is use for assing Undefined value to variable  
+// 6. Null        let y = Null  It's Stanalone value, representation of empty value
+// 7. Symbol      Uniqness
+// 8. Object      
+
+let id=1;
+let name="Ram"
+let isLoggedIn = true
+let x=null
+
+console.log(typeof(id));
+
+console.log(typeof(name));
+
+console.log(typeof(isLoggedIn));
+
+console.log(typeof(x));     // "typeof" method is use to know the actual type of variable, what is the type of varible like Number,String or Boolean,etc.
+
+
+
