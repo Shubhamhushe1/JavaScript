6baef7b7a3317ecfca00a71706bb633c7a20954f
Shubham Hushe <101410703+Shubhamhushe1@users.noreply.github.com>
2023-12-23 06:16:52 +0000

    variables basics

---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 123;
+let accountName = "Shubham H";
+var accountType = "Savings";
+branchname = "SBIXYZ";
+city=
+
+// const is use for storing constant value 
+// let is use for if we dont want to set constant value.
+// Prefer not to use ** var ** 
+// We dont use **var** these days because it's lack of scope, is supports on older version browsers and  Functional Scope.
+
+console.log(accountId);
+console.table([accountId,accountName,accountType,branchname,city]);
