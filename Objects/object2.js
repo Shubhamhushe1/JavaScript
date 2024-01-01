561e6bf926a395d4a660e4ac037547ce38e6b543
Shubham Hushe <101410703+Shubhamhushe1@users.noreply.github.com>
2024-01-01 06:46:21 +0000

    Objects Destructure and JSON Introductions

--- Objects/object2.js
+++ Objects/object2.js
@@ -90,3 +90,47 @@ console.log(users[1]);  //      Output =>     { no: 2, naam: 'B' }   // using in
 console.log(users[2]);  //      Output =>     { no: 3, naam: 'C' }
 
 users[0].id // we can get specific details too
+
+                                              
+
+                                                        // Object Destructure 
+
+const subject = {
+
+    coursename: "JavaScript",
+    teacher : "Hitesh",
+    price : 999
+    
+    }
+
+
+// console.log(subject.coursename);   // Output => JavaScript   // we can get output like this but we can destructure the  object 
+
+// const {coursename : course} = subject                       //Destructuring             
+// console.log(course);              // Output => JavaScript    
+
+const{coursename} = subject
+console.log(coursename);         // we can destructure like this too 
+
+                                                            
+                                                                // JSON 
+
+// Example of JSON 
+
+{
+    "employees":[
+        {"firstName":"John", "lastName":"Doe"},
+        {"firstName":"Anna", "lastName":"Smith"},
+        {"firstName":"Peter", "lastName":"Jones"}
+      ]
+
+}
+
+//JSON is a format for storing and transporting data. JSON is often used when data is sent from a server to a web page.
+//What is JSON?
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data interchange format
+// JSON is language independent *
+// JSON is "self-describing" and easy to understand
+// * The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text only. Code for reading and generating JSON data can be written in any programming language.
+
